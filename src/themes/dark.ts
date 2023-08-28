bf156d91035e7a58e549eb7ec4c1df0e2f2c0de3
Andre <andreituassu3@gmail.com>
2023-08-28 20:02:52 -0300

    configurando o botao de troca de tema

---
+++ src/themes/dark.ts
@@ -0,0 +1,18 @@
+const temaDark = {
+  corPrincipal: '#FFF',
+  corSecundaria: '#eee',
+  corDeFundo: '#282A35',
+  corTextoBotao: '#FFF',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
---
+++ src/themes/light.ts
@@ -0,0 +1,10 @@
+const temaLight = {
+  corPrincipal: '#282A35',
+  corSecundaria: '#949494',
+  corDeFundo: '#FFF',
+  corTextoBotao: '#FFF',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaLight
